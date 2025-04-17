8993573a31cba714a188767bbb6dd9ee348aae69
Prathmesh <prathmeshthorat725@gmail.com>
2025-04-17 13:58:54 +0530

    Initial commit for Node.js app

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(3002, () => {
-  console.log("Server is running on http://localhost:3000");
+  console.log("Server is running on http://localhost:3002");
 });
