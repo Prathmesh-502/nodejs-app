b602cd6c7365ff807cb8fc3dc61d64e9c8a63c3f
Prathmesh-502 <prathmeshthorat502@gmail.com>
2025-04-17 13:56:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const server = http.createServer((req, res) => {
   res.end("Hello from Node.js via Jenkins!");
 });
 
-server.listen(3000, () => {
+server.listen(3002, () => {
   console.log("Server is running on http://localhost:3000");
 });
